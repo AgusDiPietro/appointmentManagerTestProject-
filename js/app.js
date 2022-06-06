e46a9d250089decbb837a7856a0b0ada0c71eb3a
AgusDiP <agusdipietro@live.com.ar>
2022-06-06 20:58:04 +0200

    Agregamos el boton y la funcion de eliminar citas y actualziamos el arreglo con las citas y la UI de las citas que quedan

--- js/app.js
+++ js/app.js
@@ -17,10 +17,11 @@ class Citas {
    }
    agrgarCita(cita) {
        this.citas = [...this.citas, cita];
-
-       console.log(this.citas);
    }
 
+   eliminarCita(id) {
+       this.citas = this.citas.filter(cita => cita.id !== id); 
+   } 
 };
 
 class UI {
@@ -84,6 +85,13 @@ class UI {
                <span class= "font-weight-bolder">Sintomas: </span> $(sintomas)
             `;
 
+            //boton para eliminar cita
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+            btnEliminar.innerHTML = 'Eliminar <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'; 
+
+            btnEliminar.onClick = () => eliminarCita(id);
+
             // Agregar los parrafos al DivCita
             divCita.appendChild(mascotaParrafo);
             divCita.appendChild(propietarioParrafo);
@@ -91,6 +99,7 @@ class UI {
             divCita.appendChild(fechaParrafo);
             divCita.appendChild(horaParrafo);
             divCita.appendChild(sintomasParrafo);
+            divCita.appendChild(btnEliminar);
 
             //Agregar las citas al HTML
             contenedorCitas.appendChild(divCita);
@@ -166,7 +175,7 @@ function nuevaCita(e){
 
     //Mostrar el HTML de las citas
     ui.imprimirCitas(administrarCitas);
-}
+};
 
 function reniciarObjeto(){
     citaObj.mascota= '';
@@ -175,4 +184,16 @@ function reniciarObjeto(){
     citaObj.fecha= '';
     citaObj.hora= '';
     citaObj.sintomas= '';
-}
+};
+
+function eliminarCita(id){
+    //Eliminar la cita
+    administrarCitas.eliminarCita(id);
+
+    //Muestre mensaje
+    ui.imprimirAlerta("La cita fue eliminada");
+
+    //Refresque las citas
+    ui.imprimirCitas(administrarCitas);
+
+};
