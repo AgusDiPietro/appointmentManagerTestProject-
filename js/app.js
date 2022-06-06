84909184d9efcb8c142b350902161702462acc6b
AgusDiP <agusdipietro@live.com.ar>
2022-06-06 20:39:51 +0200

    Agregamos las citas al HTML dandole formato y limpiando el html para que no aparezca dos veces la misma cita

--- js/app.js
+++ js/app.js
@@ -46,6 +46,62 @@ class UI {
             divMensaje.remove();
         }, 2000);
     }
+
+    imprimirCitas({citas}){
+
+        this.limpiarHTML();
+        
+        citas.forEach(cita => {
+            const {mascota, propietario, telefono, fecha, hora, sintomas, id} = cita;
+
+            const divCita = document.createElement('div');
+            divCita.classList.add('cita', 'p-3');
+            divCita.dataset.id = id;
+
+            //Scripting de los elementos de la cita
+            const mascotaParrafo = document.createElement('h2');
+            mascotaParrafo.classList.add('card-title', 'font-weight-bolder');
+            mascotaParrafo.textContent = mascota;
+
+            const propietarioParrafo = document.createElement('p');
+            propietarioParrafo.innerHTML = `
+               <span class= "font-weight-bolder">Propietario: </span> $(propietario)
+            `;
+            const telefonoParrafo = document.createElement('p');
+            telefonoParrafo.innerHTML = `
+               <span class= "font-weight-bolder">Telefono: </span> $(telefono)
+            `;
+            const fechaParrafo = document.createElement('p');
+            fechaParrafo.innerHTML = `
+               <span class= "font-weight-bolder">Fecha: </span> $(fecha)
+            `;
+            const horaParrafo = document.createElement('p');
+            horaParrafo.innerHTML = `
+               <span class= "font-weight-bolder">Hora: </span> $(hora)
+            `;
+            const sintomasParrafo = document.createElement('p');
+            sintomasParrafo.innerHTML = `
+               <span class= "font-weight-bolder">Sintomas: </span> $(sintomas)
+            `;
+
+            // Agregar los parrafos al DivCita
+            divCita.appendChild(mascotaParrafo);
+            divCita.appendChild(propietarioParrafo);
+            divCita.appendChild(telefonoParrafo);
+            divCita.appendChild(fechaParrafo);
+            divCita.appendChild(horaParrafo);
+            divCita.appendChild(sintomasParrafo);
+
+            //Agregar las citas al HTML
+            contenedorCitas.appendChild(divCita);
+        })
+    }
+    //limpiar HTML para que no haya dos veces la misma cita
+    limpiarHTML(){
+        while(contenedorCitas.firstChild){
+            contenedorCitas.removeChild(contenedorCitas.firstChild);
+        }
+    }
 };
 
 // las instanciamos globalmente
@@ -104,10 +160,12 @@ function nuevaCita(e){
 
     // Reiniciamos el obj
     reniciarObjeto(); 
-    
+
     //reniciar formulario despues de mandar una cita
     formulario.reset();
 
+    //Mostrar el HTML de las citas
+    ui.imprimirCitas(administrarCitas);
 }
 
 function reniciarObjeto(){
